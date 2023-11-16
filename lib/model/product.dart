da06398f0e9eff8c2a4db038c90d1d0862f98663
viettungvuong <viettungvuong@gmail.com>
2023-11-16 16:17:14 +0700

    Persistent dâata

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -3,21 +3,52 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class Product{
+  late int id;
   late String image;
   late String name;
   late String description;
   late double price;
   late String colorHex;
 
-  Product(this.image, this.name, this.description, this.price, this.colorHex);
+  Product(this.id, this.image, this.name, this.description, this.price, this.colorHex);
+
+  Product.fromJson(Map<String, dynamic> json)
+  {
+    id = json['id'] as int;
+    image = json['image'] as String;
+    name = json["name"] as String;
+    description = json["description"] as String;
+    price = json["price"] as double;
+    colorHex = json["color"] as String;
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'image': image,
+    'name': name,
+    'description': description,
+    'price': price,
+    'color': colorHex
+  };
+
 }
 
 class ProductInCart extends Product{
   int _quantity = 0;
 
-  ProductInCart(super.image, super.name, super.description, super.price, super.colorHex);
+  ProductInCart(super.id, super.image, super.name, super.description, super.price, super.colorHex);
 
-  ProductInCart.withProduct(Product product) : super(product.image, product.name, product.description, product.price, product.colorHex);
+  ProductInCart.withProduct(Product product) : super(product.id, product.image, product.name, product.description, product.price, product.colorHex);
+
+  ProductInCart.fromJson(Map<String, dynamic> json): super.fromJson(json)
+  {
+    _quantity = json['quantity'] as int;
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'quantity': _quantity
+  };
 
   void updateQuantity(int quantity){
     _quantity = quantity;
@@ -31,4 +62,6 @@ class ProductInCart extends Product{
   double calculatePrice(){
     return price * _quantity;
   }
+
+
 }
--- lib/providers.dart
+++ lib/providers.dart
@@ -1,7 +1,9 @@
 //a singleton class that contains neccessary data
+import 'dart:convert';
 import 'dart:core';
 
 import 'package:riverpod/riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'model/product.dart';
 
@@ -12,3 +14,25 @@ final cartProvider = StateProvider<List<ProductInCart>>((ref) {
 final productProvider = StateProvider<List<Product>>((ref) {
   return [];
 });
+
+//save for persistence
+Future<void> saveCartItems(List<ProductInCart> cart) async {
+  final prefs = await SharedPreferences.getInstance();
+  final cartItemsJson = jsonEncode(cart);
+  await prefs.setString('cart_items', cartItemsJson);
+}
+
+//load for persistence
+Future<List<ProductInCart>> loadCartItems() async {
+  final prefs = await SharedPreferences.getInstance();
+  final cartItemsJson = prefs.getString('cart_items');
+  if (cartItemsJson != null) {
+    final cartItems = jsonDecode(cartItemsJson) as List<dynamic>;
+    return cartItems.map((cartItemJson) => ProductInCart.fromJson(cartItemJson)).toList();
+  } else {
+    return [];
+  }
+}
+
+
+
